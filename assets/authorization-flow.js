a87d6f9b908a72e923f792c6007ee84a76efb3cc
dn-lx <don.alex.perera@gmail.com>
2026-09-22 23:02:28 +0200

    Fix retired authorization script syntax

--- assets/authorization-flow.js
+++ assets/authorization-flow.js
@@ -1,1 +1,4 @@
-(()=>{\n  // Legacy 48-hour authorization override retired.\n  // The active FrankiHolz v2 payment flow is implemented in site.js / admin-5.js.\n})();\n
+(()=>{
+  // Legacy 48-hour authorization override retired.
+  // The active FrankiHolz v2 payment flow is implemented in site.js / admin-5.js.
+})();
